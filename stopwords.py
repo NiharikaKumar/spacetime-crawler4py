9a07216f7d485ad139340a9f6eee66c412de9087
Seongjin Yoon <seongjiy@circinus-9.ics.uci.edu>
2024-05-03 09:57:47 -0700

    updated stopwords

--- stopwords.py
+++ stopwords.py
@@ -35,8 +35,9 @@ stopwords = ['a', "a's", 'able', 'about', 'above', 'according', 'accordingly', '
 'whatever', 'whats', 'when', "when's", 'whence', 'whenever', 'where', "where's", 'whereafter', 'whereas', 'whereby', 'wherein', 'wheres', 'whereupon', 'wherever', 
 'whether', 'which', 'while', 'whim', 'whither', 'who', "who'll", "who's", 'whod', 'whoever', 'whole', 'whom', 'whomever', 'whos', 'whose', 'why', "why's", 'widely', 
 'will', 'willing', 'wish', 'with', 'within', 'without', "won't", 'wonder', 'wont', 'words', 'world', 'would', "wouldn't", 'wouldnt', 'www', 'x', 'y', 'yes', 'yet', 
+'prev', 'technical', 'size', 'image', 'ori', 'original', 'details', 'iso', 'canon', 'eos', 'equiv', 'crw', 'usm', 'crop', 'olympus', 'sara', 'timothy', 'david',
 'you', "you'd", "you'll", "you're", "you've", 'youd', 'your', 'youre', 'yours', 'yourself', 'yourselves', 'z', 'zero', 'image', 
 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december',
 'jan', 'feb', 'mar', 'apr', 'jun', 'jul', 'aug', 'sep', 'sept', 'oct', 'nov', 'dec', 
 "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
-'prev', 'technical', 'size', 'image', 'ori']
+"mon", "tue", "wed", "thu", "fri", "sat", "sun", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
